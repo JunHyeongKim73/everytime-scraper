f5998ec4ffb97600abe11c587a039456b6ee0d5a
JunHyeongKim73 <rla5764v@gmail.com>
2022-01-12 16:59:56 +0900

    코드 올리기

---
+++ execute.sh
@@ -0,0 +1,5 @@
+while true :
+do
+    python everytime_crawler.py
+    sleep 3600
+done
---
+++ test.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+now = datetime.now()
+print(f'{now.day}일 {now.hour}시 {now.minute}분')
